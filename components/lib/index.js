fcf9f794fd6d4a9a810c72116681d53917e073fa
pei_lu <1521937232@qq.com>
2022-11-02 11:01:39 +0800

    feat: 初始化代码结构

--- components/lib/index.js
+++ components/lib/index.js
@@ -1,23 +1,18 @@
-import demo from "./demo/src/demo.vue"
-import card from "./card/src/card.vue"
+import demo from './demo/src/demo.vue';
+import card from './card/src/card.vue';
 
+const components = [demo, card];
 
-const components = [demo, card]
+const install = Vue => {
+	// 避免重复注册
+	if (install.installed) return;
 
-const install = (Vue) => {
-  // 避免重复注册
-  if (install.installed) return;
-  console.log(Vue)
-  // Object.keys(components).forEach((key) => {
-  //   Vue.components(components[key].name, components[key])
-  // })
-  components.forEach((item) => {
-    Vue.component(item.name, item)
-  })
-
-}
+	components.forEach(item => {
+		Vue.component(item.name, item);
+	});
+};
 const API = {
-  install
-}
+	install,
+};
 
 export default API;
